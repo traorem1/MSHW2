3bcd92fc33fff343f8e57581edd2665c69d2910b
traorem1 <traorem1@nku.edu>
2015-01-29 11:07:21 -0500

    Original project
    Copying the original project

---
+++ src/com/merge/conflict/Hello.java
@@ -0,0 +1,13 @@
+package com.merge.conflict;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Team member 1: a");
+		System.out.println("Team member 1: b");
+		System.out.println("Team member 2: 1");
+		System.out.println("Team member 2: 2");
+		System.out.println("Team member changes this line entirely");
+	}
+	
+}
