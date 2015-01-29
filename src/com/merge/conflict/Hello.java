425cd2dad55c033859d9c664f6734006fea46408
traorem1 <traorem1@nku.edu>
2015-01-29 16:56:57 -0500

    Tease team 1
    Tease team 1

--- src/com/merge/conflict/Hello.java
+++ src/com/merge/conflict/Hello.java
@@ -2,12 +2,12 @@ package com.merge.conflict;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		System.out.println("Team member 1: a");
-		System.out.println("Team member 1: b");
-		System.out.println("Team member 2: 1");
-		System.out.println("Team member 2: 2");
-		System.out.println("Team member changes this line entirely");
-	}
-	
+ public static void main(String[] args) { 
+   System.out.println("Team member 1: a"); 
+   System.out.println("Team member 1: b"); 
+   System.out.println("Team member 2: 1"); 
+   System.out.println("Team member 2: 2"); 
+   System.out.println("Team member changes this line entirely"); 
+
+   }
 }
